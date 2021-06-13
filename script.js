e0d2e8edfd45f13933839a85b72a0e11687204ab
Ted Heikkila <ted.heikkila@gmail.com>
2021-06-12 19:41:44 -0500

    selected correct arrays, concatenated, trying to randomize, then splice

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// JS for password generator, TWH, 6/12/21
+
+
+// Initializing global arrays (LC, UC, nums, and special chars)
+lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+specChars = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", "," ,"-", ".", "/", ":", ";", "<", "=", ">", "?","@", "[", "]", "^","_","`", "{", "|", "}", "~"]
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -7,39 +16,95 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-    // Asks user for password length
+  
+    // Asks user for password length and sets the array length. Stores in passwordLength variable
     function generatePassword() {
       var passwordLength = window.prompt("How many characters (8-128) would you like for your password's length?");
 
-    // If user pressed Cancel, immediately end function
-    if (!passwordLength) {
-    return;
-    }
+      // If user pressed Cancel at start, immediately end function
+      if (!passwordLength) {
+      return;
+      }
 
-    // For loop creates recursion for multiple entries of bad data
-    // If user enters in numerical value >= 8 and <= 128 continue on; else re-prompt for various scenarios
-    var i = 0
-    do {
-      if (passwordLength>=8 && passwordLength<=128) {
-        i++;
-      } else if (passwordLength===null) {
-        i++
-      } else if (passwordLength<8) {
-        passwordLength = window.prompt("Please enter a value greater than or equal to 8");
-      } else if (passwordLength>128) {
-        passwordLength = window.prompt("Please enter a value less than or equal to 128");
-      } else {
-        passwordLength = window.prompt("Please enter a numerical value between 8 and 128");
-      } 
-    } while (i<1)
+      // Do while loop creates recursion for multiple entries of bad data
+      // If user enters in numerical value >= 8 and <= 128 or hits "Cancel" at any point in loop continue on; else re-prompt for various scenarios
+      var i = 0
+      do {
+        if (passwordLength>=8 && passwordLength<=128) {
+          i++;
+        } else if (passwordLength===null) {
+          i++
+        } else if (passwordLength<8) {
+          passwordLength = window.prompt("Please enter a value greater than or equal to 8");
+        } else if (passwordLength>128) {
+          passwordLength = window.prompt("Please enter a value less than or equal to 128");
+        } else {
+          passwordLength = window.prompt("Please enter a numerical value between 8 and 128");
+        } 
+      } while (i<1)
 
-    // Create the letter, numeric, and special char arrays; If statements to confirm use of LC's, UC's, numeric, and special chars
-    // The inputted number sets the array size; each if inserts specified data type 
 
-    console.log(passwordLength)
+      // If statements to add arrays of LC's, UC's, numeric, and special chars
+      // Get random array by using random sort function
+      // Use splice to cut array down based upon specified pw length
+      // ok prompt means using that array (true); cancel means not using array (false)
     
+      var charConfirm = window.confirm("The next four screens are for character type selection. Selecting more OK's will result in a stronger password (please select at least one). Click OK to continue.")
 
+      if (!charConfirm) {
+        return;
+      }
+
+      //passwordArray, passwordLength, char type selectors as if statements
+      var lowerCase2 = window.confirm("(1) Include lowercase? (Click OK to use lowercase letters. Click Cancel to not use lowercase letters)")
+      var upperCase2 = window.confirm("(2) Include uppercase? (Click OK to use uppercase letters. Click Cancel to not use uppercase letters)")
+      var numbers2 = window.confirm("(3) Include numbers? (Click OK to use numbers. Click Cancel to not use numbers)")
+      var specChars2 = window.confirm("(4) Include special characters? (Click OK to use special characters. Click Cancel to not use special characters)")
+
+
+      // Using OK's/Cancels to include/not include relevant arrays; emptied array for each using else's if selected cancel (returns false)
+        if (lowerCase2 === true || lowerCase2 ===false && upperCase2 ===false && numbers2 ===false && specChars2===false) {
+        lowerCase2 = lowerCase 
+      } else lowerCase2 = []
+      
+      if (upperCase2 === true) {
+        upperCase2 = upperCase
+      } else upperCase2 = []
+      
+      if (numbers2 === true) {
+        numbers2 = numbers
+      } else numbers2 = []
+      
+      if (specChars2 === true) {
+        specChars2 = specChars
+      } else specChars2 = []
+      
+
+      //Concat the resulting arrays
+      var passwordArray = lowerCase2.concat(upperCase2, numbers2, specChars2);
+
+      console.log(passwordArray.length)
+
+      //Randomly sort this array
+      function randomPasswordArray() {
+        for (let i = passwordArray.length -1; i > 0; i--) {
+            let j = Math.floor(Math.random() * i)
+            let k = passwordArray[i]
+            passwordArray[i] = passwordArray[j]
+            passwordArray[j] = k
+          }  
+      }
+      
+      var randomPassword = randomPasswordArray(passwordArray)
+
+      console.log(randomPassword)
+
+    //Splice down to size of entered value
+
+    //Turn the final array into a string and display it
+
+
+    
 
 
 
